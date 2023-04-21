b4f887fe87020b31d4d2adf4027cc37c48c1d3b5
Takahiro Tsuchiya <crsk.plmzh@gmail.com>
2023-04-21 13:39:14 +0900

    fix uploadFromUrlToS3

--- src/upload.test.ts
+++ src/upload.test.ts
@@ -1,10 +1,25 @@
-import { expect, test } from 'vitest'
+import { test } from 'vitest'
 import { uploadFromUrlToS3 } from "./upload";
+import { S3Client } from "@aws-sdk/client-s3";
 
-// ローカルでのみテストできるように通常はskipするよ
-test('uploadFromUrlToS3', async () => {
+test.skip('uploadFromUrlToS3', async () => {
+  require('dotenv').config()
+
+  // s3clientを作成
+  const s3client = new S3Client({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    }
+  })
+
+  // new Date を用いて、YYYY-MM-DDTHH:mm:ss のファイル名を生成
+  const uploadedFilename = new Date().toISOString().replace(/:/g, '-')
+
+  // アップロード実行
   await uploadFromUrlToS3(
+    s3client,
     'https://pbs.twimg.com/profile_images/1612971754489778177/ZsuW-mWs_400x400.jpg',
-    'test.jpg'
+    `test/${uploadedFilename}.jpg`
   )
 })
--- src/upload.ts
+++ src/upload.ts
@@ -1,48 +1,33 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
-import { Readable } from "stream";
+import { Readable } from "node:stream";
 import axios from "axios";
 
-require('dotenv').config()
-
-const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
-}
-
-const s3client = new S3Client({
-  region: 'ap-northeast-1',
-  credentials: credentials,
-})
-
-export const uploadFromUrlToS3 = async (url: string, key: string,): Promise<void> => {
+export const uploadFromUrlToS3 = async (
+  s3client: S3Client,
+  url: string,
+  key: string,
+): Promise<void> => {
   try {
     console.log("Downloading file...");
-    const fileStream = await downloadFile(url);
+    const response = await axios.get(url, { responseType: "stream" });
+    const fileStream: Readable = response.data;
 
-    // see https://www.npmjs.com/package/@aws-sdk/lib-storage
     const upload = new Upload({
       client: s3client,
       params: {
-        Bucket: `${process.env.AWS_S3_BUCKET}`, // whatever your bucket is in S3
-        Key: key, // file name
-        Body: fileStream, // Body is stream which enables streaming
+        Bucket: `${process.env.AWS_S3_BUCKET}`,
+        Key: key,
+        Body: fileStream,
+        ContentType: "image/jpg",
       },
     });
-
-    // アクセスコントロールの制御が必要かも
 
     console.log("Uploading to S3...");
     await upload.done();
 
     console.log("Upload successful!");
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 };
-
-const downloadFile = async (url: string): Promise<Readable> => {
-  const response = await axios.get(url, { responseType: "stream" });
-  return response.data;
-};
-
